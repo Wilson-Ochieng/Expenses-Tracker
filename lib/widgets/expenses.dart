7e38000e0c163538317dc02f1082975ab87547bf
Wilson Owino <37038018+Wilson-Ochieng@users.noreply.github.com>
2025-04-14 19:36:36 +0300

    Added icons

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,49 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+        Expense(
+        title: 'Cinema',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+        Expense(
+        title: 'Eatery',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.food),
+        Expense(
+        title: 'Picnic',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.travel),
+  ];
+
+  @override
+  Widget build(context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+          const  Text('The Chart'),
+         Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}

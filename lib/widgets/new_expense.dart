d225680e3d64512af712c1c21ae56d88122ff1c3
Wilson Owino <37038018+Wilson-Ochieng@users.noreply.github.com>
2025-04-15 13:00:25 +0300

    Added input  and  saving data logic

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
+  
+
   @override
   State<NewExpense> createState() {
     return _NewExpenseState();
@@ -10,17 +12,40 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+
+void _saveTitleInput(String  inputValue){
+  _enteredTitle = inputValue;
+
+
+
+
+
+
+
+
+}
+
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
+    return   Padding(
       padding:   EdgeInsets.all(16),
       child: Column(
         children:  [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration:  const InputDecoration(
               label: Text('Title'),
             ),
+          ),
+          Row(
+            children: [
+ 
+              ElevatedButton(onPressed:(){
+                print('$_enteredTitle');
+              }, child: Text('Save Expense'),)
+            ],
           )
         ],
       ),
